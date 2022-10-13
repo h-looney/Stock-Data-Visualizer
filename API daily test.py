781bd85844a37fabd14e39b9f8cb0afd17b6119d
CheeseFan40 <89537997+CheeseFan40@users.noreply.github.com>
2022-10-13 13:28:10 -0500

    Update API daily test.py

--- API daily test.py
+++ API daily test.py
@@ -1,9 +1,14 @@
 import requests
+import pygal
 
 ##pulled from documentation for reference with API in use
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&interval=5min&apikey=AU4UXQO6WFYENR4J'
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&interval=5min&apikey=AU4UXQO6WFYENR4J'
 r = requests.get(url)
 data = r.json()
 
-print(data)
+#print(data)
+dates, open, high, low, close = [], [], [], []
 
+
+for point in data: 
+    dates.append
