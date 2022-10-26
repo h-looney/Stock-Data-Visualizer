d641d074ab0fc5cb923e85180889a76bed7f185d
Jules Parham <parhamjules@pop-os.localdomain>
2022-10-25 23:39:55 -0500

    created StockDataChart class

--- helpers.py
+++ helpers.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 
 DF_DEFAULT = '%Y-%m-%d'
+DF_INTRADAY = '%Y-%m-%d'
 
 
 def str_to_date(v, f=DF_DEFAULT):
     return datetime.strptime(v, f)
+
+
+def date_to_str(dt, f=DF_DEFAULT):
+    return dt.strftime(f)
 
 
 def is_valid_date(v):
